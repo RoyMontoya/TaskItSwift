449271e42e778226922f82e5c431e366c5005c35
RoyMontoya <roy_a18@hotmail.com>
2016-03-09 23:39:05 -0700

    set first steps for tableView, prototype for row

---
+++ Taskit/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  Taskit
+//
+//  Created by Roy Montoya on 3/9/16.
+//  Copyright © 2016 Roy Montoya. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
